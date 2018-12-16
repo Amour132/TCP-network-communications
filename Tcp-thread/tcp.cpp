47a1644605bc37cc252d1a9402aad253e088d50c
Amour132 <1046692639@qq.com>
2018-12-16 10:40:51 +0800

    Tcp-thread

--- Tcp-thread/tcp.cpp
+++ Tcp-thread/tcp.cpp
@@ -15,7 +15,7 @@ void Usage()
   cout << "usage: ./server [ip] [port]" << endl;
 }
 
-void ProcessRequest(int client_fd,struct sockaddr_int* client_addr)
+void ProcessRequest(int client_fd,struct sockaddr_in* client_addr)
 {
   char buf[1024] = {0};
   for(;;)
@@ -27,13 +27,12 @@ void ProcessRequest(int client_fd,struct sockaddr_int* client_addr)
     }
     if(read_size == 0)
     {
-      cout << "client " << inet_ntoa(client_addr->sin_addr) << "say bye!" << endl;
+      cout << "client: " << inet_ntoa(client_addr->sin_addr) << "say bye!" << endl;
       close(client_fd);
       break;
     }
     buf[read_size] = '\0';
-    cout << "client " << inet_ntoa(client_addr->sin_addr) 
-      <<"say: " << buf << endl;
+    cout << "client: " << inet_ntoa(client_addr->sin_addr) <<"say: " << buf << endl;
     write(client_fd,buf,strlen(buf));
   }
   return;
@@ -44,3 +43,61 @@ typedef struct Arg
   int fd;
   struct sockaddr_in addr;
 }Arg;
+
+void *CreatWorker(void *ptr)
+{
+  Arg* arg = (Arg*)ptr;
+  ProcessRequest(arg->fd,&(arg->addr));
+  free(arg);
+  return NULL;
+}
+
+int main(int argc,char* argv[])
+{
+  if(argc != 3)
+  {
+    Usage();
+    exit(1);
+  }
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(atoi(argv[2]));
+  addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+  int fd = socket(AF_INET,SOCK_STREAM,0);
+  if(fd < 0)
+  {
+    exit(1);
+  }
+
+  int ret = bind(fd,(struct sockaddr*)&addr,sizeof(addr));
+  if(ret < 0)
+  {
+    exit(1);
+  }
+
+  ret = listen(fd,10);
+  if(ret < 0)
+  {
+    exit(1);
+  }
+
+  for(;;)
+  {
+    struct sockaddr_in client_addr;
+    socklen_t len = sizeof(client_addr);
+    int client_fd = accept(fd,(struct sockaddr*)&client_addr,&len);
+    if(client_fd < 0)
+    {
+       continue;
+    }
+    pthread_t tid = 0;
+    Arg* arg = new Arg;
+    arg->fd = client_fd;
+    arg->addr = client_addr;
+    pthread_create(&tid,NULL,CreatWorker,(void*)arg);
+    pthread_detach(tid);
+
+  }
+  return 0;
+}
